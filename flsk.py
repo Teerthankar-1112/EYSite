24d227ee8a2cd75c2f64ca70018226dbde82cc8f
Teerthankar-1112 <91729455+Teerthankar-1112@users.noreply.github.com>
2023-01-19 17:11:51 +0530

    Update flsk.py

--- flsk.py
+++ flsk.py
@@ -1,22 +1,8 @@
-import os
 import sqlite3
 import pandas as pd
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
-
-app.config["DEBUG"] = True
-
-UPLOAD_FOLDER = 'static/files'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-mydb = sqlite3.connect("dataset.db")
-
-mcr = mydb.cursor()
-
-for x in mcr:
-    print(x)
-
 
 @app.route('/')
 def index():
