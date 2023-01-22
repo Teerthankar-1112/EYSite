45b5ec1cc2483b7ec1e1c63cc9f5d01696493f7a
Teerthankar-1112 <91729455+Teerthankar-1112@users.noreply.github.com>
2023-01-22 16:17:03 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,53 +1,17 @@
 import sqlite3
-import pandas as pd
-from flask import Flask, render_template, request, redirect, url_for
 
-app = Flask(__name__)
-
-app.config["DEBUG"] = True
-
-UPLOAD_FOLDER = r'static\files'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-mydb = sqlite3.connect("dataset.db")
-
-mcr = mydb.cursor()
-
-for x in mcr:
-    print(x)
-@app.route('/')
-def index():
-    return render_template("index.html")
+connection = sqlite3.connect('database.db')
 
 
-@app.route("/", methods=['POST'])
-def uploadFiles():
-    uploaded_file = request.files['file']
-    if uploaded_file.filename != '':
-        # file_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
-        # uploaded_file.save(file_path)
-        parseCSV(uploaded_file)
-    return redirect(url_for('index.html'))
+with open('schema.sql') as f:
+    connection.executescript(f.read())
+
+cur = connection.cursor()
+
+cur.execute("INSERT INTO data (CustomerId, EmailId, CustomerName, CreditScore, Location, Gender, Age, NoOfTransactions, Balance, NoOfProducts, HasCrCard, IsActMember, TotalTransactAmt, EstdSalary, Exited, TransactDate) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+            ('Data', 'CSV File for dataframe')
+            )
 
 
-def parseCSV(filePath):
-    col_names = ['CustomerId', 'EmailId', 'CustomerName', 'CreditScore', 'Location', 'Gender', 'Age',
-                 'NoOfTransactions', 'Balance', 'NoOfProducts', 'HasCrCard', 'IsActMember', 'TotalTransactAmt',
-                 'EstdSalary', 'Exited', 'TransactDate']
-    df = pd.read_csv(filePath, names=col_names, header=None)
-    for i, row in df.iterrows():
-        sql = "INSERT INTO addresses (CustomerId, EmailId, CustomerName, CreditScore, Location, Gender, Age, NoOfTransactions, Balance, NoOfProducts, HasCrCard, IsActMember, TotalTransactAmt, EstdSalary, Exited, TransactDate) VALUES (%d, %s, %s, %f, %s, %s, %f, %f, %f, %f, %f, %f, %f, %f, %f, %s)"
-        value = (
-            row['CustomerId'], row['EmailId'], row['CustomerName'], row['CreditScore'], row['Location'], row['Gender'],
-            row['Age'], row['NoOfTransactions'], row['Balance'], row['NoOfProducts'], row['HasCrCard'],
-            row['IsActMember'],
-            row['TotalTransactAmt'], row['EstdSalary'], row['Exited'], row['TransactDate'])
-        mcr.execute(sql, value)
-        mydb.commit()
-        print(i, row['CustomerId'], row['EmailId'], row['CustomerName'], row['CreditScore'], row['Location'],
-              row['Gender'], row['Age'], row['NoOfTransactions'], row['Balance'], row['NoOfProducts'], row['HasCrCard'],
-              row['IsActMember'], row['TotalTransactAmt'], row['EstdSalary'], row['Exited'], row['TransactDate'])
-
-
-if __name__ == "__main__":
-    app.run(port=5000)
+connection.commit()
+connection.close()
