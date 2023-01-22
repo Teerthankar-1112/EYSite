17004508743d13c9d749b05361623bd4eca6181e
Teerthankar-1112 <91729455+Teerthankar-1112@users.noreply.github.com>
2023-01-22 16:24:24 +0530

    Update flsk.py

--- flsk.py
+++ flsk.py
@@ -1,42 +1,97 @@
 import sqlite3
-import pandas as pd
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, url_for, flash, redirect
+from werkzeug.exceptions import abort
 
-app = Flask(__name__)
 
-@app.route('/')
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
+
+
+application = Flask(__name__)
+application.config['SECRET_KEY'] = 'secretsecretsecretkey'
+
+
+@application.route('/')
 def index():
-    return render_template("index.html")
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
 
 
-@app.route("/", methods=['POST'])
-def uploadFiles():
-    uploaded_file = request.files['file']
-    if uploaded_file.filename != '':
-        # file_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
-        # uploaded_file.save(file_path)
-        parseCSV(uploaded_file)
-    return redirect(url_for('index.html'))
+@application.route('/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
+    return render_template('post.html', post=post)
 
 
-def parseCSV(filePath):
-    col_names = ['CustomerId', 'EmailId', 'CustomerName', 'CreditScore', 'Location', 'Gender', 'Age',
-                 'NoOfTransactions', 'Balance', 'NoOfProducts', 'HasCrCard', 'IsActMember', 'TotalTransactAmt',
-                 'EstdSalary', 'Exited', 'TransactDate']
-    df = pd.read_csv(filePath, names=col_names, header=None)
-    for i, row in df.iterrows():
-        sql = "INSERT INTO addresses (CustomerId, EmailId, CustomerName, CreditScore, Location, Gender, Age, NoOfTransactions, Balance, NoOfProducts, HasCrCard, IsActMember, TotalTransactAmt, EstdSalary, Exited, TransactDate) VALUES (%d, %s, %s, %f, %s, %s, %f, %f, %f, %f, %f, %f, %f, %f, %f, %s)"
-        value = (
-            row['CustomerId'], row['EmailId'], row['CustomerName'], row['CreditScore'], row['Location'], row['Gender'],
-            row['Age'], row['NoOfTransactions'], row['Balance'], row['NoOfProducts'], row['HasCrCard'],
-            row['IsActMember'],
-            row['TotalTransactAmt'], row['EstdSalary'], row['Exited'], row['TransactDate'])
-        mcr.execute(sql, value)
-        mydb.commit()
-        print(i, row['CustomerId'], row['EmailId'], row['CustomerName'], row['CreditScore'], row['Location'],
-              row['Gender'], row['Age'], row['NoOfTransactions'], row['Balance'], row['NoOfProducts'], row['HasCrCard'],
-              row['IsActMember'], row['TotalTransactAmt'], row['EstdSalary'], row['Exited'], row['TransactDate'])
+# Important Create Post Function --------------------------------
+@application.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                         (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
+
+
+# Edit Post Function -------
+
+@application.route('/<int:id>/edit', methods=('GET', 'POST'))
+def edit(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE posts SET title = ?, content = ?'
+                         ' WHERE id = ?',
+                         (title, content, id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('edit.html', post=post)
+
+
+# The Delete Function -----------------------------------
+@application.route('/<int:id>/delete', methods=('POST',))
+def delete(id):
+    post = get_post(id)
+    conn = get_db_connection()
+    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+    flash('"{}" was successfully deleted!'.format(post['title']))
+    return redirect(url_for('index'))
 
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    application.run()
